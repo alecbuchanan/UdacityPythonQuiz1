e5ed89dc8de667d76e8962790328418a240c62a8
alecbuchanan <alec.s.buchanan@gmail.com>
2017-04-18 12:13:02 -0400

    Add files via upload
    First Python Quiz

---
+++ UdactiyPythonQuiz1/Quiz1.py
@@ -0,0 +1,5 @@
+# Write a Python program that prints out the number of minutes in seven weeks.
+# Remember: 7 weeks 7 days in a week, 24 hours in a day, and 60 mins in an hour.
+# Multiplying these numbers together will give you the result
+
+print (7 * 7 * 24 * 60)
